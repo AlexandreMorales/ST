cc98e061583f9e20277a8038d2dffa8ff0cc55ea
AlexandreMorales <xandemorales@gmail.com>
2015-03-14 19:19:16 -0300

    #3 - Adicionado setUp() e variaveis de frequencias e notas

--- src/st/ST.java
+++ src/st/ST.java
@@ -2,8 +2,11 @@ package st;
 
 import java.util.HashMap;
 import java.util.Map;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.TargetDataLine;
 import javax.swing.JOptionPane;
@@ -11,13 +14,131 @@ import javax.swing.JOptionPane;
 public class ST extends javax.swing.JFrame {
 
     Map<String, Line.Info> mapaMicrofones;
+    Map<Double, String> mapaFrequencias;
+    TargetDataLine targetDataLine;
+    private static final double[] FREQUENCIES = {146.83, 138.59, 130.81, 123.47, 116.54, 110.00, 103.83, 98.00, 92.50, 87.31, 82.41, 77.78};
+    private static final String[] NAME = {"D", "C#", "C", "B", "A#", "A", "G#", "G", "F#", "F", "E", "D#"};
 
     public ST() {
         initComponents();
         this.setLocationRelativeTo(null);
         carregarMicrofones();
+        setUp(mapaMicrofones.get((String) cbMicrofones.getSelectedItem()));
+    }
+
+    public void setUp(Line.Info lineInfo) {
+        try {
+            float sampleRate = 44100;
+            int sampleSizeInBits = 16;
+            int channels = 1;
+            boolean signed = true;
+            boolean bigEndian = false;
+            AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+            DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, format);
+            targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+
+            targetDataLine.open(format, (int) sampleRate);
+            targetDataLine.start();
+
+            byte[] buffer = new byte[2 * 1200];
+            int[] a = new int[buffer.length / 2];
+
+            int n = -1;
+            while ((n = targetDataLine.read(buffer, 0, buffer.length)) > 0) {
+                for (int i = 0; i < n; i += 2) {
+                    int value = (short) ((buffer[i] & 0xFF) | ((buffer[i + 1] & 0xFF) << 8));
+                    a[i >> 1] = value;
+                }
+                double prevDiff = 0;
+                double prevDx = 0;
+                double maxDiff = 0;
+                int sampleLen = 0;
+                int len = a.length / 2;
+                for (int i = 0; i < len; i++) {
+                    double diff = 0;
+                    for (int j = 0; j < len; j++) {
+                        diff += Math.abs(a[j] - a[i + j]);
+                    }
+                    double dx = prevDiff - diff;
+                    if (dx < 0 && prevDx > 0) {
+                        if (diff < (0.1 * maxDiff)) {
+                            if (sampleLen == 0) {
+                                sampleLen = i - 1;
+                            }
+                        }
+                    }
+                    prevDx = dx;
+                    prevDiff = diff;
+                    maxDiff = Math.max(diff, maxDiff);
+                }
+                if (sampleLen > 0) {
+                    double frequency = (format.getSampleRate() / sampleLen);
+                    
+                    /*freqLabel.setText(String.format("%.2fhz", frequency));
+                    frequency = normaliseFreq(frequency);
+                    int note = closestNote(frequency);
+                    matchLabel.setText(NAME[note]);
+                    System.out.println(NAME[note]);
+                    prevLabel.setText(NAME[note - 1]);
+                    nextLabel.setText(NAME[note + 1]);
+
+                    int value = 0;
+                    double matchFreq = FREQUENCIES[note];
+                    if (frequency < matchFreq) {
+                        double prevFreq = FREQUENCIES[note + 1];
+                        value = (int) (-FREQ_RANGE * (frequency - matchFreq) / (prevFreq - matchFreq));
+                    } else {
+                        double nextFreq = FREQUENCIES[note - 1];
+                        value = (int) (FREQ_RANGE * (frequency - matchFreq) / (nextFreq - matchFreq));
+                    }
+                    freqSlider.setValue(value);
+                } else {
+                    matchLabel.setText("--");
+                    prevLabel.setText("--");
+                    nextLabel.setText("--");
+                    freqSlider.setValue(0);
+                    freqLabel.setText("--");
+                }
+                prevLabel.setSize(prevLabel.getPreferredSize());
+                nextLabel.setSize(nextLabel.getPreferredSize());
+                matchLabel.setSize(matchLabel.getPreferredSize());
+
+                freqSlider.repaint();
+                freqLabel.repaint();*/
+                }
+                try {
+                    Thread.sleep(250);
+                } catch (Exception e) {
+                }
+            }
+
+        } catch (LineUnavailableException lue) {
+            JOptionPane.showMessageDialog(null, lue.getMessage());
+        }
     }
     
+    private void criaFreq(){
+        mapaFrequencias = new HashMap <Double, String>();
+        for (int i = 0; i < 5; i++) {
+            for (int j = NAME.length-1; j >0; j--) {
+                mapaFrequencias.put(FREQUENCIES[j]*(i^2), NAME[j]);
+            }
+        }
+    }
+    
+    private static int closestNote(double hz) {
+        double minDist = Double.MAX_VALUE;
+        int minFreq = -1;
+        for (int i = 0; i < FREQUENCIES.length; i++) {
+            double dist = Math.abs(FREQUENCIES[i] - hz);
+            if (dist < minDist) {
+                minDist = dist;
+                minFreq = i;
+            }
+        }
+        return minFreq;
+    }
+
     public void carregarMicrofones() {
         cbMicrofones.setModel(new javax.swing.DefaultComboBoxModel(new String[]{}));
         try {
@@ -89,7 +210,7 @@ public class ST extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cbMicrofonesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbMicrofonesActionPerformed
-        
+        setUp(mapaMicrofones.get((String) cbMicrofones.getSelectedItem()));
     }//GEN-LAST:event_cbMicrofonesActionPerformed
 
     public static void main(String args[]) {
